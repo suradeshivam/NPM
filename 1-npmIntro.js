993cf29b22824c9a796832c00fd1185c6db4a066
shivam surade <suradeshivamshyam@gmail.com>
2023-08-02 17:20:40 +0530

    first commit

---
+++ 1-npmIntro.js
@@ -0,0 +1,31 @@
+// npm calls reusable code as package 
+// a package consist of multiple js files
+
+// npm -> global command comes with node
+// npm --version
+
+
+// local dependency -> use it only in perticular project
+// npm i <packageName>
+
+// global dependency ->use it in any project
+// npm install -g <packageName>
+
+// package.json -> manifest file (stores important info about project / package)
+// manual approach -> create package.json in the root, create properties etc
+// npm init -> step by step, press enter to skip
+// npm init -y -> everything default
+
+// dependencies are stored into node_modules folder
+// if any dependency uses another package then node will install that package also
+
+
+// using a package
+
+const _ = require('lodash')
+
+const arr = [1,[2,[3,[4]]]]
+
+const newArr = _.flattenDeep(arr)
+
+console.log(newArr)
---
+++ 2-git.js
@@ -0,0 +1,12 @@
+// why package.json is important and git
+
+// the size of the node_modules contains the dependencies is high
+// so when we upload our project to github we ignore the node_modules folder by specifying the node_modules in .gitignore file
+// when the another user download the project from the repo 
+// the package.json file contains the dependencies list
+// so user can use 'node install' command and all the dependencies will get installed
+
+// How to add project to ther repo
+// first create a repo let's say name 'NPM'
+// create a .gitignore file to ignore the folder
+//
